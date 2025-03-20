5f5049c79653288309e46298008213bc1617194d
Jared Heinrich <jared.heinrich@sap.com>
2025-03-20 08:29:58 +0100

    Implement funktionality for neural network
    Neural network can be initialized with random weights between -1.0 -
    1.0 and 0.0 biases.
    
    The network can be used to calculate the output vector from the input
    vector. This is done by calculating step by step the output vector for
    each layer by weights[layer] * input_vector + biases[layer] =
    output_vector. The calculated output vector is then used as input vector
    for the next layer until all layers are processed.

--- neural_network/src/neural_network.rs
+++ neural_network/src/neural_network.rs
@@ -10,7 +10,46 @@ impl NeuralNetwork {
     /// 0      : input layer
     /// 1..n-1 : hidden layers
     /// n      : output layer
+    ///
+    /// creates a neural network from given layer sizes.
+    ///
+    /// 311 will look something like this:
+    ///
+    ///   0 0 1 1: indices in weights and biases list
+    /// 0 \
+    /// 0 - 1 - 2
+    /// 0 /
+    ///
+    /// Layer 0 is the input layer and wont be stored in the neural network.
+    /// But it is needed to create the weight matrix between layer 0 and layer 1.
+    /// In this scenario the matrices look like the following.
+    ///
+    /// weights[0]  biases[0]  weights[1]  biases[1]
+    /// 
+    ///   x x x        x           x          x
+    ///
+    /// The goal is that weights[0] * input_layer + biases[0] will result in the output values from
+    /// layer 1
+    ///
     pub fn new(layers: Vec<usize>) -> Self {
-        todo!("not implemented")
+        let (weights, biases): (Vec<Matrix>, Vec<Matrix>) = layers.windows(2).map(|window| {
+            let prev_nodes: usize = window[0];
+            let current_nodes: usize = window[1];
+            (Matrix::random(current_nodes, prev_nodes), Matrix::zero(current_nodes, 1))
+        }).unzip();
+        NeuralNetwork { layers, weights, biases }
+    }
+
+    /// Calculates the output values for specific input values.
+    /// For each Layer the next output values are calculated this way:
+    /// weights * input_values + biases
+    pub fn process(&self, input_values: Matrix) -> Matrix {
+        assert_eq!(input_values.columns, 1);
+        assert_eq!(input_values.rows, self.layers[1]);
+        let mut result = input_values;
+        self.weights.iter().zip(self.biases.iter()).for_each(|(weight_matrix, bias_matrix)| {
+            result = weight_matrix.multiply(&result).unwrap().add(bias_matrix).unwrap();
+        });
+        result
     }
 }
