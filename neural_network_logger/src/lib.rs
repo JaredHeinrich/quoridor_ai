5e02fb8acda7cc696921c8a4f57ebd912abd2dd1
Valentin <v.spam@cze-kalla.de>
2025-04-09 22:03:55 +0200

    alphabetic ordering in lib.rs

--- neural_network_logger/src/lib.rs
+++ neural_network_logger/src/lib.rs
@@ -1,7 +1,7 @@
 // This file defines the public interface for the logging functionality of the neural network logger crate.
 
+pub mod error;
 pub mod logger;
 pub mod models;
-pub mod error;
 
-pub use logger::{log_single_network, log_generation, read_log_entries};
+pub use logger::{log_generation, log_single_network, read_log_entries};
