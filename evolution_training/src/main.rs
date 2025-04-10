13afa47825185659b9b74f69cec83964b92408d6
Valentin <v.spam@cze-kalla.de>
2025-04-11 00:30:34 +0200

    initialized modular file structure

--- evolution_training/src/main.rs
+++ evolution_training/src/main.rs
@@ -1,2 +1,20 @@
 pub mod training_environment;
-fn main() {}
+pub mod settings;
+pub mod logging;
+
+pub mod evolution {
+    pub mod generation;
+    pub mod selection;
+    pub mod r#mod;
+}
+
+pub mod game_adapter {
+    pub mod board_encoder;
+    pub mod move_decoder;
+    pub mod reward;
+    pub mod mod;
+}
+
+fn main() {
+    
+}
