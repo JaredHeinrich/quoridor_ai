6f25972edac64f186b628becdc9c76f02afc2e63
Valentin <v.spam@cze-kalla.de>
2025-04-13 14:59:43 +0200

    made the changes of file renames in main

--- evolution_training/src/main.rs
+++ evolution_training/src/main.rs
@@ -1,19 +1,18 @@
 pub mod training_environment;
 pub mod settings;
-pub mod logging;
 pub mod error;
 
 pub mod evolution {
     pub mod generation;
     pub mod selection;
-    pub mod r#mod;
+    pub mod lib;
 }
 
 pub mod game_adapter {
     pub mod board_encoder;
     pub mod move_decoder;
     pub mod reward;
-    pub mod r#mod;
+    pub mod lib;
 }
 
 fn main() {
