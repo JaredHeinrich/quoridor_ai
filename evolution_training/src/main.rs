cdaf76b2bba83368a968eb7157971d45d89b62fa
Valentin <v.spam@cze-kalla.de>
2025-04-13 16:43:07 +0200

    added agent struct

--- evolution_training/src/main.rs
+++ evolution_training/src/main.rs
@@ -1,19 +1,8 @@
 pub mod training_environment;
 pub mod settings;
 pub mod error;
-
-pub mod evolution {
-    pub mod generation;
-    pub mod selection;
-    pub mod lib;
-}
-
-pub mod game_adapter {
-    pub mod board_encoder;
-    pub mod move_decoder;
-    pub mod reward;
-    pub mod lib;
-}
+pub mod evolution;
+pub mod game_adapter;
 
 fn main() {
     
