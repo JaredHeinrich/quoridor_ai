d16049a1ba97402083db352a250f0c7c242e7bff
Valentin <v.spam@cze-kalla.de>
2025-04-14 05:03:40 +0200

    formatted

--- evolution_training/src/lib.rs
+++ evolution_training/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod error;
 pub mod evolution;
 pub mod game_adapter;
+pub mod logging;
 pub mod settings;
 pub mod training_environment;
-pub mod logging;
--- evolution_training/src/main.rs
+++ evolution_training/src/main.rs
@@ -1,8 +1,8 @@
 pub mod error;
 pub mod evolution;
 pub mod game_adapter;
+pub mod logging;
 pub mod settings;
 pub mod training_environment;
-pub mod logging;
 
 fn main() {}
